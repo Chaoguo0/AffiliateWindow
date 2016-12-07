a8f9efbe47897f399d18108e8eb11f20c1d4eb77
ChaoG <ChaoG@DESKTOP-P11S7M7>
2016-12-07 17:26:00 -0500

    Added Show Products

--- AffiliateWindow/Form1.Designer.cs
+++ AffiliateWindow/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.btnShowResult = new System.Windows.Forms.Button();
             this.lblMerchants = new System.Windows.Forms.Label();
+            this.lblProductLists = new System.Windows.Forms.Label();
+            this.btnShowProductLists = new System.Windows.Forms.Button();
             this.lblProducts = new System.Windows.Forms.Label();
             this.btnShowProducts = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -54,33 +56,54 @@
             this.lblMerchants.Text = "Placeholder for merchants";
             this.lblMerchants.Click += new System.EventHandler(this.lblResult_Click);
             // 
+            // lblProductLists
+            // 
+            this.lblProductLists.AutoSize = true;
+            this.lblProductLists.Location = new System.Drawing.Point(284, 62);
+            this.lblProductLists.Name = "lblProductLists";
+            this.lblProductLists.Size = new System.Drawing.Size(137, 13);
+            this.lblProductLists.TabIndex = 2;
+            this.lblProductLists.Text = "Placeholder for product lists";
+            // 
+            // btnShowProductLists
+            // 
+            this.btnShowProductLists.Location = new System.Drawing.Point(287, 341);
+            this.btnShowProductLists.Name = "btnShowProductLists";
+            this.btnShowProductLists.Size = new System.Drawing.Size(217, 53);
+            this.btnShowProductLists.TabIndex = 0;
+            this.btnShowProductLists.Text = "Show Product Lists Results";
+            this.btnShowProductLists.UseVisualStyleBackColor = true;
+            this.btnShowProductLists.Click += new System.EventHandler(this.lblResult_Click);
+            // 
             // lblProducts
             // 
             this.lblProducts.AutoSize = true;
-            this.lblProducts.Location = new System.Drawing.Point(502, 62);
+            this.lblProducts.Location = new System.Drawing.Point(579, 61);
             this.lblProducts.Name = "lblProducts";
             this.lblProducts.Size = new System.Drawing.Size(122, 13);
-            this.lblProducts.TabIndex = 2;
+            this.lblProducts.TabIndex = 4;
             this.lblProducts.Text = "Placeholder for products";
             // 
             // btnShowProducts
             // 
-            this.btnShowProducts.Location = new System.Drawing.Point(505, 341);
+            this.btnShowProducts.Location = new System.Drawing.Point(582, 341);
             this.btnShowProducts.Name = "btnShowProducts";
             this.btnShowProducts.Size = new System.Drawing.Size(217, 53);
-            this.btnShowProducts.TabIndex = 0;
-            this.btnShowProducts.Text = "Show Products Results";
+            this.btnShowProducts.TabIndex = 5;
+            this.btnShowProducts.Text = "Show Product Results";
             this.btnShowProducts.UseVisualStyleBackColor = true;
-            this.btnShowProducts.Click += new System.EventHandler(this.lblResult_Click);
+            this.btnShowProducts.Click += new System.EventHandler(this.btnShowProducts_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1013, 470);
-            this.Controls.Add(this.lblProducts);
-            this.Controls.Add(this.lblMerchants);
             this.Controls.Add(this.btnShowProducts);
+            this.Controls.Add(this.lblProducts);
+            this.Controls.Add(this.lblProductLists);
+            this.Controls.Add(this.lblMerchants);
+            this.Controls.Add(this.btnShowProductLists);
             this.Controls.Add(this.btnShowResult);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -93,6 +116,8 @@
 
         private System.Windows.Forms.Button btnShowResult;
         private System.Windows.Forms.Label lblMerchants;
+        private System.Windows.Forms.Label lblProductLists;
+        private System.Windows.Forms.Button btnShowProductLists;
         private System.Windows.Forms.Label lblProducts;
         private System.Windows.Forms.Button btnShowProducts;
     }
--- AffiliateWindow/Form1.cs
+++ AffiliateWindow/Form1.cs
@@ -13,24 +13,23 @@ namespace AffiliateWindow
 {
     public partial class Form1 : Form
     {
+
+        Awin.ApiPortTypeClient client;
+        UserAuthentication userAuthentication;
+
         public Form1()
         {
             InitializeComponent();
-        }
 
-        Awin.ApiPortTypeClient client = new Awin.ApiPortTypeClient();
-        UserAuthentication userAuthentication = new UserAuthentication();
-        userAuthentication.sApiKey = "b9e01904105aac94b7ded008abcef422";
-
-
-
-        private void btnShowResult_Click(object sender, EventArgs e)
-        {
             Awin.ApiPortTypeClient client = new Awin.ApiPortTypeClient();
             UserAuthentication userAuthentication = new UserAuthentication();
             userAuthentication.sApiKey = "b9e01904105aac94b7ded008abcef422";
 
-            getMerchant merchant = new getMerchant();
+        }
+        
+        private void btnShowResult_Click(object sender, EventArgs e)
+        {
+                        getMerchant merchant = new getMerchant();
 
             // comment out following lines to get all merchants …
             merchant.iMerchantId = new int[5];
@@ -42,6 +41,7 @@ namespace AffiliateWindow
             // … until here
 
             Merchant[] result = client.getMerchant(userAuthentication, merchant);
+            
             lblMerchants.Text = "Merchants (id, name):\n";
 
             foreach (var item in result)
@@ -54,24 +54,50 @@ namespace AffiliateWindow
 
         private void lblResult_Click(object sender, EventArgs e)
         {
+            Awin.ApiPortTypeClient client = new Awin.ApiPortTypeClient();
+            UserAuthentication userAuthentication = new UserAuthentication();
+            userAuthentication.sApiKey = "b9e01904105aac94b7ded008abcef422";
 
+            getProductList product = new getProductList();
+
+            product.sQuery = "Real Madrid Home";
+            product.iLimit = 5;
+
+            lblProductLists.Text = "Products (id, name):\n";
+
+            getProductListResponse result = new getProductListResponse();
+
+            result.oProduct = new Product[100];
+                
+            result =client.getProductList(userAuthentication, product);
+
+            lblProductLists.Text += "Products in total:" + result.iTotalCount.ToString() + "\n";
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                lblProductLists.Text += result.oProduct[i].sName + "\n";
+
+            }
+        }
+
+        private void btnShowProducts_Click(object sender, EventArgs e)
+        {
+            Awin.ApiPortTypeClient client = new Awin.ApiPortTypeClient();
+            UserAuthentication userAuthentication = new UserAuthentication();
+            userAuthentication.sApiKey = "b9e01904105aac94b7ded008abcef422";
 
             getProduct product = new getProduct();
 
             product.iProductId = new long[1];
-            product.iProductId[0] = 1122223;
 
-            Product[] result = client.getProduct(userAuthentication, product);
+            product.iProductId [0] = 3389364729;
+
             lblProducts.Text = "Products (id, name):\n";
 
-            foreach (var item in result)
-            {
-                lblProducts.Text += item.iId.ToString() + ", " + item.sName + "\n";
-            }
-
-
-
-
+            Product[] result = client.getProduct(userAuthentication, product);
+            
+            lblProducts.Text +=   result[0].sName + "\n";
 
         }
     }
